9868f82e1e2b030e026ea964e32e7e1fb6c781d9
kim.jeongin <kim.jeongin@navercorp.com>
2018-12-18 15:14:04 +0900

    #3 vuejs 기본 프로젝트 구조 추가

---
+++ web/frontend/vue.config.js
@@ -0,0 +1,9 @@
+const path = require('path');
+module.exports = {
+  baseUrl: '/',
+  outputDir: path.resolve(__dirname, "../src/main/resources/static"),
+  runtimeCompiler: undefined,
+  productionSourceMap: undefined,
+  parallel: undefined,
+  css: undefined
+}
